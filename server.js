d23dc67f4f1e8b04d452df7e628fad9bc1382496
Tech-Aryan <aryan.mirajkar98@gmail.com>
2025-02-12 00:12:36 +0530

    Replaced bcrypt with bcryptjs for Render deployment

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const express = require('express');
 const http = require('http');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const bcrypt = require('bcrypt');
+const bcrypt = require("bcryptjs");
+
 const jwt = require('jsonwebtoken');
 
 // Constants
